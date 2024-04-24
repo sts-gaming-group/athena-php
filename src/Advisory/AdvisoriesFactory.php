3f853c3dd2be4685fd51eaacc2287e61aba88369
Filip Szymanski <filip.szymanski@sts.pl>
2024-04-24 15:10:36 +0200

    fix: validating if output array is not null before generating advisories arr

--- src/Advisory/AdvisoriesFactory.php
+++ src/Advisory/AdvisoriesFactory.php
@@ -13,38 +13,39 @@ class AdvisoriesFactory
     }
     public function create(string $auditOutput): AdvisoryList
     {
-        $data = json_decode($auditOutput, true);
-
         $advisories = [];
-        foreach ($data['advisories'] as $packageName => $advisoriesData) {
-            foreach ($advisoriesData as $advisoryData) {
-                $sources = [];
-                foreach ($advisoryData['sources'] as $sourceData) {
-                    $source = new Source(
-                        $sourceData['name'],
-                        $sourceData['remoteId'],
+        $data = json_decode($auditOutput, true);
+        if ($data) {
+            foreach ($data['advisories'] as $packageName => $advisoriesData) {
+                foreach ($advisoriesData as $advisoryData) {
+                    $sources = [];
+                    foreach ($advisoryData['sources'] as $sourceData) {
+                        $source = new Source(
+                            $sourceData['name'],
+                            $sourceData['remoteId'],
+                        );
+
+                        $sources[] = $source;
+                    }
+
+                    $advisory = new Advisory(
+                        $advisoryData['advisoryId'],
+                        $advisoryData['packageName'],
+                        $advisoryData['affectedVersions'],
+                        $advisoryData['title'],
+                        $advisoryData['cve'],
+                        $advisoryData['link'],
+                        new \DateTime($advisoryData['reportedAt']),
+                        $sources
                     );
 
-                    $sources[] = $source;
+                    if ($advisory->hasCve()) {
+                        $metrics = $this->metricsApi->get($advisory->cve);
+                        $advisory->setMetrics($metrics);
+                    }
+
+                    $advisories[] = $advisory;
                 }
-
-                $advisory = new Advisory(
-                    $advisoryData['advisoryId'],
-                    $advisoryData['packageName'],
-                    $advisoryData['affectedVersions'],
-                    $advisoryData['title'],
-                    $advisoryData['cve'],
-                    $advisoryData['link'],
-                    new \DateTime($advisoryData['reportedAt']),
-                    $sources
-                );
-
-                if ($advisory->hasCve()) {
-                    $metrics = $this->metricsApi->get($advisory->cve);
-                    $advisory->setMetrics($metrics);
-                }
-
-                $advisories[] = $advisory;
             }
         }
 
